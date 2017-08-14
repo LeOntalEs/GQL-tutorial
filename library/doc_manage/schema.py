8d296fb55a6975cce0abce24540f045dc6cc9b08
taratep <taratep.sira@gmail.com>
2017-08-14 23:29:23 +0700

    library: add user to schema

--- library/doc_manage/schema.py
+++ library/doc_manage/schema.py
@@ -1,12 +1,18 @@
 from graphene import relay, ObjectType, AbstractType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-
+from django.contrib.auth.models import User
 from doc_manage.models import Document, Borrow, Category
 
 
-# Graphene will automatically map the Category model's fields onto the CategoryNode.
-# This is configured in the CategoryNode's Meta class (as you can see below)
+class UserNode(DjangoObjectType):
+    class Meta:
+        model = User
+        filter_fields = {
+            'username': ['exact', 'icontains', 'istartswith']
+        }
+        interfaces = (relay.Node, )
+
 class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
@@ -17,7 +23,6 @@ class CategoryNode(DjangoObjectType):
 class DocumentNode(DjangoObjectType):
     class Meta:
         model = Document
-        # Allow for some more advanced filtering here
         filter_fields = {
             'title': ['exact', 'icontains', 'istartswith'],
             'category': ['exact'],
@@ -28,7 +33,6 @@ class DocumentNode(DjangoObjectType):
 class BorrowNode(DjangoObjectType):
     class Meta:
         model = Borrow
-        # Allow for some more advanced filtering here
         filter_fields = {
             'borrower__username': ['exact', 'icontains', 'istartswith'],
         }
@@ -36,11 +40,14 @@ class BorrowNode(DjangoObjectType):
 
 
 class Query(AbstractType):
+    users = relay.Node.Field(UserNode)
+    all_users = DjangoFilterConnectionField(UserNode)
+
     category = relay.Node.Field(CategoryNode)
     all_categories = DjangoFilterConnectionField(CategoryNode)
 
     documents = relay.Node.Field(DocumentNode)
     all_documents = DjangoFilterConnectionField(DocumentNode)
 
-    borrow = relay.Node.Field(BorrowNode)
-    all_borrow = DjangoFilterConnectionField(BorrowNode)
+    borrows = relay.Node.Field(BorrowNode)
+    all_borrows = DjangoFilterConnectionField(BorrowNode)
